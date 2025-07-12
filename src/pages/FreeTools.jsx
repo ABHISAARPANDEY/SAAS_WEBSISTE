b7418ea1d4b7073c609c833edef934aa1d4c9b3d
ABHISAARPANDEY <pandey.abhisaar22@gmail.com>
2025-07-13 02:24:52 +0530

    pro

--- src/pages/FreeTools.jsx
+++ src/pages/FreeTools.jsx
@@ -561,9 +561,9 @@ const FreeTools = () => {
       </section>
 
       {/* Search and Filters */}
-      <section className="py-12 bg-dark-secondary border-b border-dark sticky top-16 z-40">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col lg:flex-row gap-6 items-center">
+<section className="py-12 bg-dark-secondary border-b border-dark relative z-10">
+  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="flex flex-col gap-6">
             {/* Search Bar */}
             <div className="flex-1 relative">
               <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-text-secondary w-5 h-5" />
